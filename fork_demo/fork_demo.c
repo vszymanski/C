e591ca65f436248d228088e8835bb70cf1a84c09
Vincent SZYMANSKI <vszymanski@protonmail.com>
2017-09-22 23:09:53 +0200

    Change-Id: I6f3ac61c4d0f7526fa75f9b112b8e5e804e44fbd Signed-off-by: Vincent SZYMANSKI <vszymanski@protonmail.com>

---
+++ fork_demo/fork_demo.c
@@ -0,0 +1,14 @@
+/*
+ * fork_demo.c
+ *
+ *  Created on: 22 sept. 2017
+ *      Author: Vincent SZYMANSKI
+ *      email : vszymanski@protonmail.com
+ */
+
+
+int main(int argc, char **argv)
+{
+	return 0
+
+}
